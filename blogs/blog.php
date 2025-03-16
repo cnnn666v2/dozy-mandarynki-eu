5a81a559a9e951cab2dd90b30bcae7bb132ed0fe
Cnnn666v2 <cnnn666@proton.me>
2025-03-17 00:45:23 +0100

    some blog viewing changes

--- blogs/blog.php
+++ blogs/blog.php
@@ -28,17 +28,23 @@
                 <div id="container-blog" class="flex flex-col items-center w-full">
                     <div id="blog-banner" class="flex flex-row justify-center items-center w-full gap-3 bg-[url('/img/jajco.png')] py-10 relative border-b-2 border-gray-500">
                         <span class="w-full h-full absolute bg-black bg-opacity-65 z-0"></span>
-                        <img src="/img/jajco.png" class="w-[50px] h-[50px] rounded-full z-10" />
+                        <img src="/img/jajco.png" class="w-[50px] h-[50px] rounded-full z-10 place-self-start" />
                         <div class="flex flex-col z-10">
                             <h1 class="font-bold w-full">This is a very example title of a blog post which is very important</h1>
-                            <div class="flex flex-row">
-                                <p>Author: Cnnn666</p>
-                                <p class="ml-auto">Published on: 3/12/2025 | Edited on: 4/12/2025</p>
+                            <div class="flex flex-col">
+                                <div class="flex flex-row">
+                                    <p>Author: Cnnn666</p>
+                                    <p class="ml-auto">Published on: 3/12/2025 | Edited on: 4/12/2025</p>
+                                </div>
+                                <div class="flex flex-row gap-3">
+                                    <button type="button" class="rounded-lg px-4 border-2 mt-2 border-cyan-600 hover:bg-cyan-600 uppercase transition-colors ease-in-out duration-200 w-max">Edit blog</button>
+                                    <button type="button" class="rounded-lg px-4 border-2 mt-2 border-red-600 hover:bg-red-600 uppercase transition-colors ease-in-out duration-200 w-max">Delete blog</button>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div id="blog-content" class="flex flex-col text-left max-w-[100vw] md:max-w-[50vw] m-5">
+                    <div id="blog-content" class="flex flex-col text-left max-w-[100vw] xl:max-w-[50vw] m-5">
                         <p>where lets say a 6.45685584123 where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123where lets say a 6.45685584123</p>
                     </div>
 
--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-let version = "1.0.0";
+let version = "1.2.0";
 let branch = "b"; //b - beta, s - stable
 
 document.getElementById("version-txt").textContent =
